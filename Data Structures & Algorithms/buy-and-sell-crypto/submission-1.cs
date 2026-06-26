757d58779fe056c99fe4db90e289d6dc6cf29a6d
Brijesh R <56794851+brijeshr-81@users.noreply.github.com>
2026-06-26 21:26:15 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        int left = 0;
+        int right = 1;
+        int maxProfit = 0;
+        while (right < prices.Length)
+        {
+            if (prices[right] > prices[left])
+            {
+                var profit = prices[right] - prices[left];
+                maxProfit = Math.Max(maxProfit, profit);
+            }
+            else
+            {
+                left = right;
+            }
+            right ++;
+        }
+
+        return maxProfit;
+    }
+}
+
